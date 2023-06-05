b6680752dec9d18a6b94fb5e84621352df625ed0
Tiya Mehta <tiyakev@gmail.com>
2023-06-05 18:07:45 +0530

    initial commit

--- src/pacman/Pacman.java
+++ src/pacman/Pacman.java
@@ -3,12 +3,9 @@ package pacman;
 import javax.swing.JFrame;
 
 public class Pacman extends JFrame{
-
 	public Pacman() {
 		add(new Model());
 	}
-	
-	
 	public static void main(String[] args) {
 		Pacman pac = new Pacman();
 		pac.setVisible(true);
@@ -18,5 +15,4 @@ public class Pacman extends JFrame{
 		pac.setLocationRelativeTo(null);
 		
 	}
-
 }
